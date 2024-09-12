0245f6880372c4a6be1803c96259edcd9c0025a2
Innocent Anozie <iygeal@gmail.com>
2024-09-12 23:30:37 +0100

    Fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,11 +30,11 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
+    connection = mysql.connector.connect(user=user,
                                    password=passwd,
                                    host=host,
                                    database=db_name)
-    return conn
+    return connection
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
