c7299d25327bf8542001256995c392d1a30f591a
Innocent Anozie <iygeal@gmail.com>
2024-09-18 15:49:51 +0100

    TASK 9:         Extracts user email and password from Base64         decoded authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,3 +53,26 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts user email and password from Base64
+        decoded authorization header.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded string.
+
+        Returns:
+            Tuple: User email and password or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string into email and password using ':'
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
