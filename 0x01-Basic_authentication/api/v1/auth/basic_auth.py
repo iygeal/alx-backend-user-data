171bfffdeb2d6698dc1620ad0e04ed4130695b09
Innocent Anozie <iygeal@gmail.com>
2024-09-18 08:55:39 +0100

    TASK 7: Extracts the Base64 encoded authorization header from the         Authorization HTTP header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,25 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 encoded authorization header from the
+        Authorization HTTP header.
+
+        Args:
+            authorization_header (str): The Authorization HTTP header.
+
+        Returns:
+            str: The Base64 encoded authorization header.
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str
+        ):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
