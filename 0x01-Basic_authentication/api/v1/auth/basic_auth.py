601e002be703a019fb077f66bd2b8fa24a9335c4
Innocent Anozie <iygeal@gmail.com>
2024-09-18 16:46:42 +0100

    TASK 10: Retrieves a User instance based on email and password.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -76,3 +78,36 @@ class BasicAuth(Auth):
         # Split the string into email and password using ':'
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password.
+
+        Args:
+            user_email (str): The user's email address.
+            user_pwd (str): The user's password.
+
+        Returns:
+            User instance if credentials are valid, None otherwise.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Search for user in the database based on email
+        user_list = User.search({"email": user_email})
+
+        if not user_list or len(user_list) == 0:
+            return None
+
+        # Assume the first user is the correct one
+        # (since email should be unique)
+        user = user_list[0]
+
+        # Verify if the password is correct
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
