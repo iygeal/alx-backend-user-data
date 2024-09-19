874e8c80782db7306e67b71b8eef43029a96d912
Innocent Anozie <iygeal@gmail.com>
2024-09-19 07:05:09 +0100

    TASK 1: This module handles session authentication for the API

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,9 @@ if auth_type == 'basic_auth':
 elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module handles session authentication for the API """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ This class handles session authentication for the API """
+
+    pass
