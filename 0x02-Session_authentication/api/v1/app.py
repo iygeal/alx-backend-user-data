b97be652cdbf201b1a00664a638edf60d30c1992
Innocent Anozie <iygeal@gmail.com>
2024-09-19 17:54:10 +0100

    TASK 5: Add /api/v1/auth_session/login/ to authentication excluded paths.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.basic_auth import BasicAuth
 
 
@@ -56,15 +55,20 @@ def before_request() -> str:
         return
 
     # Define paths that don't require authentication
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'  # New excluded path
+    ]
 
     # Check if the request path requires authentication
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    # Check if the Authorization header exists
-    if auth.authorization_header(request) is None:
+    # Check if both authorization header and session cookie are missing
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)  # Unauthorized
 
     # Check if the current user is authenticated
