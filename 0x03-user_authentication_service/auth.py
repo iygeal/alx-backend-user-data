cd4996ec663ae22f2c696bdbd2c860af8496b5c4
Innocent Anozie <iygeal@gmail.com>
2024-09-24 19:49:29 +0100

    TASK 4: Hashes a password using bcrypt's hashing algo and     returns hashed password as bytes

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Auth module that handles authentication related tasks"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt's hashing algo and
+    returns hashed password as bytes
+
+    Args:
+        password (str): The password to hash
+
+    Returns:
+        bytes: The hashed password
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password usin the generated salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
