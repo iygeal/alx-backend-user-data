a5947994eb29bcd9366108e3f1595618c2e82c84
Innocent Anoziee <iygeal@gmail.com>
2024-09-25 16:06:06 +0100

    FIX: Refactored auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,25 +12,21 @@ class Auth:
     """Auth class to interact with the authentication database."""
 
     def __init__(self):
-        """Initialize the Auth instance with a database instance"""
+        """Initialize the Auth instance with a database instance."""
         self._db = DB()
 
     def _hash_password(self, password: str) -> bytes:
         """Hashes a password using bcrypt's hashing algo and
-        returns hashed password as bytes
+        returns hashed password as bytes.
 
         Args:
-            password (str): The password to hash
+            password (str): The password to hash.
 
         Returns:
-            bytes: The hashed password
+            bytes: The hashed password.
         """
-        # Generate a salt
-        salt: bytes = bcrypt.gensalt()
-
-        # Hash the password usin the generated salt
+        salt: bytes = bcrypt.gensalt()  # Generate a salt
         hashed_password: bytes = bcrypt.hashpw(password.encode('utf-8'), salt)
-
         return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
@@ -48,12 +44,10 @@ class Auth:
             ValueError: If the user already exists with the given email.
         """
         try:
-            # Check if the user already exists by searching by email
             existing_user = self._db.find_user_by(email=email)
             if existing_user:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            # No user found with the email, proceed with registration
             hashed_password = self._hash_password(password)
             new_user = self._db.add_user(
                 email, hashed_password.decode('utf-8'))
@@ -73,11 +67,11 @@ class Auth:
             # Find user by email
             user = self._db.find_user_by(email=email)
 
-            # Compare the provided password with the stored hashed password
+            # Check if the provided password matches the stored hashed password
             if bcrypt.checkpw(password.encode(
                     'utf-8'), user.hashed_password.encode('utf-8')):
                 return True
             return False
-        except Exception:
-            # Return Flase if no user is found with the provided email
+        except NoResultFound:
+            # Return False if no user is found with the provided email
             return False
