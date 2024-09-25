93e71a7c5a7fdc255f88bfd87bd0779c85973dd2
Innocent Anozie <iygeal@gmail.com>
2024-09-25 23:40:09 +0100

    TASK 10: Generate session ID for a user and return it

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,3 +82,20 @@ class Auth:
         except NoResultFound:
             # Return False if no user is found with the provided email
             return False
+
+    def create_session(self, email: str) -> str:
+        """Generate session ID for a user and return it.
+
+        Args:
+            email (str): The user's email.
+
+        Returns:
+            str: The session ID or None if the user is not found.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
