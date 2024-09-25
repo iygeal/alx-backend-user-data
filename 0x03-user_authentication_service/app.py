3ed32983adf05cc992da32fb96a8eb36faa24308
Innocent Anozie <iygeal@gmail.com>
2024-09-25 23:18:12 +0100

    FIX: Deleted typo in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,5 +37,5 @@ def register_user() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-if __name__ == "__main__":4s
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
