cc565628ae3049cd8f2cec02de73668edb468aed
Innocent Anozie <iygeal@gmail.com>
2024-09-26 08:15:21 +0100

    FIX: Changed type annotations by importing Union from typing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -100,7 +101,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Returns the User associated with the given session ID.
 
         Args:
