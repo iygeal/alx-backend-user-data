5291da7d6f5cb0a46a8e2c13c65ddc5660034440
Innocent Anoziee <iygeal@gmail.com>
2024-09-26 16:25:38 +0100

    TASK 17: POST /reset_password route handler to generate reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -113,5 +113,22 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=['POST'])
+def reset_password() -> str:
+    """POST /reset_password route handler to generate reset password token"""
+    email = request.form.get('email')
+
+    try:
+        # Generate reset password token using AUTH
+        reset_token = AUTH.get_reset_password_token(email)
+
+        # Respond with user's email and reset token
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+
+    except ValueError:
+        # If user is not found, respond with 403 error
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
